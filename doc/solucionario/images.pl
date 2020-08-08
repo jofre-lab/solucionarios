f067f99c308d3bc6d698068770868a7bb8fb8236
Leonardo Andrés Jofré Flor <leonardo.jofre@lab-ia.com>
2020-08-08 14:59:32 -0400

    problema 1.2

--- doc/solucionario/images.pl
+++ doc/solucionario/images.pl
@@ -8,13 +8,60 @@ $cached_env_img{$key} = q|<IMG
  SRC="|."$dir".q|img1.png"
  ALT="$A,B,C$">|; 
 
-$key = q/{displaymath}Pleft(bigcup_{n=1}^{infty}A_{n}right)leqsum_{n=1}^{infty}Pleft(A_{n}right){displaymath};MSF=1.6;AAT/;
+$key = q/B_{k}=bigcup_{n=k}^{infty}A_{n};MSF=1.6;AAT/;
 $cached_env_img{$key} = q|<IMG
- WIDTH="182" HEIGHT="55" BORDER="0"
+ WIDTH="107" HEIGHT="33" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img3.png"
+ ALT="$B_{k}=\bigcup_{n=k}^{\infty}A_{n}$">|; 
+
+$key = q/displaystyle=Pleft(bigcup_{n=1}^{k}A_{n}cupB_{k+1}right);MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="162" HEIGHT="67" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img5.png"
+ ALT="$\displaystyle =P\left(\bigcup_{n=1}^{k}A_{n}\cup B_{k+1}\right)$">|; 
+
+$key = q/displaystyle=lim_{ktoinfty}sum_{n=1}^{k}Pleft(A_{n}right)+Pleft(B_{k+1}right);MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="213" HEIGHT="67" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img8.png"
+ ALT="$\displaystyle =\lim_{k\to\infty}\sum_{n=1}^{k}P\left(A_{n}\right)+P\left(B_{k+1}\right)$">|; 
+
+$key = q/displaystyle=sum_{n=1}^{infty}Pleft(A_{n}right)+lim_{ktoinfty}Pleft(B_{k+1}right);MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="213" HEIGHT="61" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img9.png"
+ ALT="$\displaystyle =\sum_{n=1}^{\infty}P\left(A_{n}\right)+\lim_{k\to\infty}P\left(B_{k+1}\right)$">|; 
+
+$key = q/displaystylePleft(bigcup_{n=1}^{infty}A_{n}right);MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="91" HEIGHT="63" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img4.png"
+ ALT="$\displaystyle P\left(\bigcup_{n=1}^{\infty}A_{n}\right)$">|; 
+
+$key = q/displaystylePleft(bigcup_{n=1}^{infty}A_{n}right)leqsum_{n=1}^{infty}Pleft(A_{n}right);MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="187" HEIGHT="63" ALIGN="MIDDLE" BORDER="0"
  SRC="|."$dir".q|img2.png"
- ALT="\begin{displaymath}
-P\left(\bigcup_{n=1}^{\infty}A_{n}\right)\leq\sum_{n=1}^{\infty}P\left(A_{n}\right)
-\end{displaymath}">|; 
+ ALT="$\displaystyle P\left(\bigcup_{n=1}^{\infty}A_{n}\right)\leq\sum_{n=1}^{\infty}P\left(A_{n}\right)
+$">|; 
+
+$key = q/displaystyleleqPleft(bigcup_{n=1}^{k}A_{n}right)+Pleft(B_{k+1}right);MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="191" HEIGHT="67" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img6.png"
+ ALT="$\displaystyle \leq P\left(\bigcup_{n=1}^{k}A_{n}\right)+P\left(B_{k+1}\right)$">|; 
+
+$key = q/displaystyleleqsum_{n=1}^{infty}Pleft(A_{n}right);MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="95" HEIGHT="61" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img10.png"
+ ALT="$\displaystyle \leq\sum_{n=1}^{\infty}P\left(A_{n}\right)$">|; 
+
+$key = q/displaystyleleqsum_{n=1}^{k}Pleft(A_{n}right)+Pleft(B_{k+1}right);MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="178" HEIGHT="67" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img7.png"
+ ALT="$\displaystyle \leq\sum_{n=1}^{k}P\left(A_{n}\right)+P\left(B_{k+1}\right)$">|; 
 
 1;
 
