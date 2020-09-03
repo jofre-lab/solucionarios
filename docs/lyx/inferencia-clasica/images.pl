8d7df987cfa9b17acefdd8ee4a5bbaff29d7a02c
Leonardo Andrés Jofré Flor <leonardo.jofre@lab-ia.com>
2020-09-03 00:13:13 -0400

    just update

---
+++ docs/lyx/inferencia-clasica/images.pl
@@ -0,0 +1,24 @@
+# LaTeX2HTML 2018 (Released Feb 1, 2018)
+# Associate images original text with physical files.
+
+
+$key = q/Eleft(Zright)=0impliesPleft(Z=0right)=1;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="214" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img3.png"
+ ALT="$E\left(Z\right)=0\implies P\left(Z=0\right)=1$">|; 
+
+$key = q/Z;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="16" HEIGHT="15" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img1.png"
+ ALT="$Z$">|; 
+
+$key = q/v.a;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="25" HEIGHT="14" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img2.png"
+ ALT="$v.a$">|; 
+
+1;
+
